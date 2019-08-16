943a085fe1f1c32e65a363293231fdf621f0c2eb
Regnareb <kun_fu_man@hotmail.com>
2019-08-17 01:15:00 +0200

    Use oauthlib
    Implemented the library oauthlib for Authorization and token generation
    Even more OOP
    Use the same data for all services but arrange them in each service Class.
    All the methods call one function for making requests

--- streammanager.py
+++ streammanager.py
@@ -8,6 +8,7 @@ import json
 import atexit
 import socket
 import ctypes
+import inspect
 import logging
 import webbrowser
 import subprocess
@@ -15,6 +16,7 @@ from contextlib import contextmanager
 
 import psutil
 import requests
+from requests_oauthlib import OAuth2Session
 
 logger = logging.getLogger(__name__)
 logging.basicConfig()
@@ -43,7 +45,8 @@ def pause_services(services):
 class ManageStream():
 
     def __init__(self):
-        self.status = ''
+        self.title = ''
+        self.description = ''
         self.process = ''
         self.config_filepath = os.path.join(os.path.dirname(__file__), 'streammanager.json')
         self.load_config()
@@ -63,28 +66,27 @@ class ManageStream():
     def create_services(self):
         self.services = []
         for service in self.config['streamservices']:
+            # Call the class dynamically
             self.services.append(getattr(sys.modules[__name__], service)(self.config['streamservices'][service]))
 
-    def update_channel(self, data):
+    def update_channel(self, title, description, category, tags):
         for service in self.services:
-            service.update_channel(data)
-
-    def update_tags(self, data):
-        for service in self.services:
-            service.update_tags(data)
+            service.update_channel(title, description, category, tags)
 
     def check_application(self):
         process = getForegroundProcess()
+        tags = self.config['appdata'].get(process.name(), {}).get('tags')
+        title = self.config['appdata'].get(process.name(), {}).get('title', self.title)
         category = self.config['appdata'].get(process.name(), {}).get('category')
+        description = self.config['appdata'].get(process.name(), {}).get('description', self.description)
         if category and process!=self.process:
-            data = {'channel': {'game': category}}
-            if self.config.get('forced_status'):
-                data['channel']['status'] = self.config['forced_status']
-            else:
-                data['channel']['status'] = self.config['appdata'].get(process.name(), {}).get('status', self.status)
-            self.update_channel(data)
-            tags = self.config['appdata'].get(process.name(), {}).get('tags')
-            self.update_tags(tags)
+            if self.config.get('forced_tags'):
+                tags = self.config['forced_tags'] + tags
+            if self.config.get('forced_title'):
+                title = self.config['forced_title']
+            if self.config.get('forced_description'):
+                description = self.config['forced_description']
+            self.update_channel(title, description, category, tags)
             self.process = process
 
     def main(self):
@@ -98,66 +100,84 @@ class ManageStream():
 class Service():
     def __init__(self, config):
         self.config = config
+        self.oauth2 = OAuth2Session(token=self.config['authorization'], client_id=self.config['client_id'], scope=self.config['scope'], redirect_uri=self.config['redirect_uri'])
         self.get_token()
+        if not self.config.get('channel_id'):
+            self.config['channel_id'] = self.get_channel_id()
+
+    def set_headers(self):
         self.headers = {
             'Client-ID': self.config['client_id'],
             'Authorization': 'OAuth ' + self.config['authorization']['access_token']
          }
 
-    def convert_expiration(self):
-        self.config['authorization']['expires_in'] = now = time.time() + self.config['authorization']['expires_in'] - 60
-
     def token_isexpired(self):
-        return time.time() > self.config['authorization']['expires_in']
+        return time.time() > self.config['authorization']['expires_at']
 
-    def get_token(self, address_code, address_token):
+    def get_token(self):
         try:
             if self.token_isexpired():
                 self.refresh_token()
-            else:
-                logger.info('No need to refresh the token')
-        except KeyError as e:
+        except KeyError:
             logger.info('Asking for an access code')
+            port = re.search(':(\d*)$', self.config['redirect_uri'])
+            port = int(port.group(1))
+            authorization_url, state = self.oauth2.authorization_url(self.config['authorization_base_url'], state=self.config['client_secret'])
             serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            serversocket.bind(('localhost', 777))
+            serversocket.bind(('localhost', port))
             serversocket.listen(5)
-            webbrowser.open(address_code)
+            webbrowser.open(authorization_url)
             while True:
                 connection, address = serversocket.accept()
-                buf = connection.recv(64)
+                buf = connection.recv(4096)
                 if buf:
                     break
-
-            code = re.search('code=(.*)&', str(buf))
+            code = re.search('code=(.*?)&', str(buf))
             code = code.group(1)
-            logger.info('The code is {}. Asking for the authorization token'.format(code))
-            address_token = address_token.format(code)
-            response = requests.post(address_token)
-            self.log_requests('request_token', address_token, response)
-            self.config['authorization'] = response.json()
-            self.convert_expiration()
+            logger.debug('The code is {}. Asking for the authorization token'.format(code))
 
-    def refresh_token(self, address):
-        logger.info('Refreshing the token')
-        response = requests.post(address)
-        self.log_requests('refresh_token', address, response)
-        self.authorization = response.json()
-        return response
+            self.config['authorization'] = self.oauth2.fetch_token(self.config['token_url'], code, include_client_id=True, client_secret=self.config['client_secret'])
+        finally:
+            self.set_headers()
 
-    def get_channel_info(self, address):
-        response = requests.get(address, headers=self.headers)
-        self.log_requests('get_channel_info', address, response)
-        return response.json()
+    def refresh_token(self):
+        self.config['authorization'] = self.oauth2.refresh_token(self.config['token_url'], **{'client_id': self.config['client_id'], 'client_secret': self.config['client_secret']})
 
-    def update_channel(self, address, data):
-        self.get_token()
-        response = requests.put(address, headers=self.headers, json=data)
-        self.log_requests('update_channel', address, response)
+    def request(self, action, address, headers=None, data=None):
+        if not headers:
+            headers = self.headers
+        action = getattr(requests, action)
+        response = action(address, headers=headers, json=data)
+        curframe = inspect.currentframe()
+        outframe = inspect.getouterframes(curframe, 2)[1][3]
+        self.log_requests(outframe, address, response)
         return response
 
     def log_requests(self, action, address, response):
         if not response:
             logger.error('{}: {} {}'.format(action, address, response.json()))
+        else:
+            logger.debug(response.json())
+
+    def get_channel_info(self, address):
+        response = self.request('get', address)
+        return response.json()
+
+    def update_channel(self, action, address, data):
+        self.get_token()
+        response = self.request(action, address, data=data)
+        return response
+
+    def update_tags(self, tags):
+        self.get_token()
+        response = self.request(action, address, data=data)
+        return response
+
+    def get_channel_id(self, address):
+        response = self.request('get', address)
+        return response.json()
+
+
 
 
 
@@ -165,36 +185,30 @@ class Twitch(Service):
     def __init__(self, config):
         super().__init__(config)
         self.name = 'Twitch'
-        self.headers = {
-            'Accept': 'application/vnd.twitchtv.v5+json',  # Twitch only
-            'Client-ID': self.config['client_id'],
-            'Authorization': 'OAuth ' + self.config['authorization']['access_token']
-         }
-        if not self.config.get('channel_id'):
-            self.config['channel_id'] = self.get_channel_id()
+
+    def set_headers(self):
+        super().set_headers()
+        self.headers['Accept'] = 'application/vnd.twitchtv.v5+json'
 
     def get_channel_info(self):
         address = 'https://api.twitch.tv/kraken/channels/{}'.format(self.config['channel_id'])
         return super().get_channel_info(address)
 
-    def update_channel(self, data):
-        address = 'https://api.twitch.tv/kraken/channels/{}'.format(self.config['channel_id'])
-        return super().update_channel(address, data)
-
-    def refresh_token(self):
-        address = 'https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token={}&client_id={}&client_secret={}'.format(self.config['refresh_token'], self.config['client_id'], self.config['client_secret'])  # ajouter ['authorization']
-        return super().refresh_token(address)
-
-    def get_token(self):
-        address = 'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id={}&redirect_uri={}&scope={}&state={}'.format(self.config['client_id'], self.config['redirect_uri'], self.config['scope'], self.config['client_secret'])
-        address_token = 'https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&code={{}}&grant_type=authorization_code&redirect_uri={}'.format(self.config['client_id'], self.config['client_secret'], self.config['redirect_uri'])
-        return super().get_token(address, address_token)
+    def update_channel(self, title, description, category, tags):
+        data = {}
+        if title:
+            data['status'] = title
+        if category:
+            data['game'] = self.config.get('assignation', {}).get(category, category)
+        if data:
+            data = {'channel': data}
+            address = 'https://api.twitch.tv/kraken/channels/{}'.format(self.config['channel_id'])
+            return super().update_channel('put', address, data)
 
     def get_channel_id(self):
         address = 'https://api.twitch.tv/kraken/users?login={}'.format(self.config['channel'])
-        response = requests.get(address, headers=self.headers)
-        self.log_requests('get_channel_id', address, response)
-        return response.json()['users'][0]['_id']
+        result = super().get_channel_id(address)
+        return result['users'][0]['_id']
 
     def get_alltags(self):
         cursor = ''
