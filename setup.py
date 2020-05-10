3ec5de0dde0608dfd554fc58895d1ba037516fac
Regnareb <kun_fu_man@hotmail.com>
2020-05-10 22:58:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
+import sys
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
+# Dependencies are automatically detected, but it might need fine tuning.
 buildOptions = dict(includes=['PySide2', 'concurrent.futures', 'keyboard', 'cherrypy', 'psutil', 'requests_oauthlib', 'oauthlib.oauth2.rfc6749.errors'],
-                    include_files=['common', 'services', ('icon.png', 'icon.png'), ('lib/bottle.py', 'lib/bottle.py'), 'data/theme', ('lib/pssuspend.exe', 'lib/pssuspend.exe')],
-                    silent =True)
+                    include_files=['common', 'services', ('lib/pssuspend.exe', 'lib/pssuspend.exe'), ('lib/pssuspend64.exe', 'lib/pssuspend64.exe'), ('icon.png', 'icon.png'), ('lib/bottle.py', 'lib/bottle.py'), ('data/theme', 'data/theme')],
+                    silent=True)
 
-import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
 
 executables = [
-    Executable('main.py', base=base, targetName='StreamManager.exe', icon='icon.png')
+    Executable(script='main.py', base=base, targetName='StreamManager.exe', icon='icon.ico')
 ]
 
 setup(name='Stream Manager',
       version = '1.0',
       description = '',
-      optimize = 2,
       options = dict(build_exe = buildOptions),
       executables = executables)
